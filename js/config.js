cdbb9db58232cc4a431cb664f2ba82ce5b6e404c
Damon Scott <damon.scott@nbcuni.com>
2014-10-08 22:29:53 -0700

    added social media round 1

--- js/config.js
+++ js/config.js
@@ -91,8 +91,8 @@ $(function() { h5u_parallelism.init({
 						itemWidth:			300,		// Default width (in px) of width
 						marginBottom:		40,			// Margin (in px) to preserve at the bottom of the viewport
 						marginTop:			0,			// Margin (in px) to preserve at the top of the viewport
-						maxRows:			3,			// Maximum number of rows
-						minRows:			1,			// Minimum number of rows
+						maxRows:			5,			// Maximum number of rows
+						minRows:			2,			// Minimum number of rows
 						padding:			10,			// Padding (in px) between items (0 = no padding)
 						paddingColor:		'#fff',		// Padding color
 						resetScroll:		true,		// If true, reel scroll will reset on page refresh
